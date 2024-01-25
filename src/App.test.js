e6951f999556169bc0c735a0a8e4874b807c7c71
Clara Gaset <claragaset4@gmail.com>
2024-01-25 12:11:37 +0100

    Adds integration tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title in black by default', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-}); 
+  const titleElement = screen.getByRole('heading', { level: 1 });
+  expect(titleElement.style.color).toBe('black');
+});
+
+
+test('renders title in red when red button is clicked', async () => {
+  const user = userEvent.setup();
+
+  // Given
+  render(<App />);
+
+  // When
+  const redButtonElement = screen.getByRole('button', { name: 'Click me red!' });
+  await user.click(redButtonElement);
+
+  // Then
+  const titleElement = screen.getByRole('heading', { level: 1 });
+  expect(titleElement.style.color).toBe('red');
+});
+
+test('renders title in red when blue button is clicked', async () => {
+  const user = userEvent.setup();
+
+  // Given
+  render(<App />);
+
+  // When
+  const blueButtonElement = screen.getByRole('button', { name: 'Click me blue!' });
+  await user.click(blueButtonElement);
+
+  // Then
+  const titleElement = screen.getByRole('heading', { level: 1 });
+  expect(titleElement.style.color).toBe('blue');
+});
