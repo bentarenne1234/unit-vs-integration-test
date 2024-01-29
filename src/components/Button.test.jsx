a77fbda870d5a06b3c8e3794c74ef75dc30209e1
Anna Kholodenko <anna.kholodenko.ext@personio.de>
2024-01-29 17:18:48 +0100

    Adds untit tests with a mock

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { Button } from './Button';
+import userEvent from '@testing-library/user-event'
 
 test('renders a button', () => {
   render(<Button/>);
@@ -16,4 +17,23 @@ test('renders the button text when the color text prop is provided', () => {
   render(<Button buttonText='PLEASE!!'/>);
   const buttonTextElement = screen.getByText(/Click me PLEASE!!!/i);
   expect(buttonTextElement).toBeInTheDocument();
+});
+
+test('invokes handleClick function when the user clicks on the button', async() => {
+  const user = userEvent.setup();
+  const handleClick = () => console.log("It has clicked!");
+  render(<Button buttonText='yellow' handleClick={handleClick}/>);
+  const button = screen.getByRole('button', { name: 'Click me yellow!' });
+  await user.click(button);
+  //expect(handleClick).toHaveBeenCalledTimes(1);
+
+});
+test('invokes handleClick (with mocked) function when the user clicks on the button', async() => {  
+  const user = userEvent.setup();
+  const mockedHandleClick = jest.fn();
+  render(<Button buttonText='yellow' handleClick={mockedHandleClick}/>);
+  const button = screen.getByRole('button', { name: 'Click me yellow!' });
+  await user.click(button);
+  expect(mockedHandleClick).toHaveBeenCalledTimes(1);
+
 });
