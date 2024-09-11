04565351f9def3f98d3c29f9f51201ff6dd86daa
bentarenne1234 <b.tarenne+otto@thoughtworks.com>
2024-09-12 00:39:47 +0200

    Refactor test with AAA parttern
    Co-authored-by: Paula Catalan <paula.catalan@thoughtworks.com>

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,40 +2,43 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import App from './App';
 
+let user;
+
 describe('App component', () => {
+  beforeEach(() => {
+    user = userEvent.setup();
+  })
+
   test('renders title in black by default', () => {
     render(<App />);
     const titleElement = screen.getByRole('heading', { level: 1 });
     expect(titleElement.style.color).toBe('black');
   });
-  
-  
+
+
   test('renders title in red when the user clicks the red button', async () => {
-    const user = userEvent.setup();
-  
-    // Given
+    // Arrange
     render(<App />);
-  
-    // When
     const redButtonElement = screen.getByRole('button', { name: 'Click me red!' });
+
+    // Act
     await user.click(redButtonElement);
-  
-    // Then
+
+    // Assert
     const titleElement = screen.getByRole('heading', { level: 1 });
     expect(titleElement.style.color).toBe('red');
   });
-  
+
   test('renders title in blue when the user clicks the blue button', async () => {
-    const user = userEvent.setup();
-  
-    // Given
+    // Arrange
     render(<App />);
-  
-    // When
     const blueButtonElement = screen.getByRole('button', { name: 'Click me blue!' });
+
+    // Act
+
     await user.click(blueButtonElement);
-  
-    // Then
+
+    // Assert
     const titleElement = screen.getByRole('heading', { level: 1 });
     expect(titleElement.style.color).toBe('blue');
   });
